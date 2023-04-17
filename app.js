115a5127901b48ec1c2891fdb7eace6baefc1648
KyleReto <40499747+KyleReto@users.noreply.github.com>
2023-04-17 12:46:14 -0400

    Real Initial Commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 app.use('/assets/', express.static(path.join(__dirname, 'assets')));
-app.use('/favicon.ico', express.static(path.join(__dirname, 'assets/favicon.ico')));
+//app.use('/favicon.ico', express.static(path.join(__dirname, 'assets/favicon.ico')));
 app.use('/', mainRouter);
 
 app.listen(config.listenPort);
--- routes/main.js
+++ routes/main.js
@@ -1,81 +1,26 @@
-const Quote = require('../models/quote.js');
-const Message = require('../models/message.js');
-const QuoteDB = require('../models/quoteDB.js');
-const ProfileDB = require('../models/profileDB.js');
 const config = require('../config.js');
 const Express = require('express');
 let urlencodedParser = Express.urlencoded({ extended: false});
 
 let router = Express.Router();
 
-router.post('/addquote', urlencodedParser, async function(req, res){
-	// If password isn't correct, give an error.
-	// Note: This isn't actually very secure, but it'll do for this purpose.
-	if (req.body.password != config.adminPass) return res.send('Error: Invalid Password');
-	let messages = [];
-	// For each message in the request
-	for (let i = 0; i < req.body.sender.length; i++){
-		// If there's no text and no embed (message is empty/invalid), continue
-		if (req.body.message[i] == '' && req.body.embed[i] == '') continue
+router.get('/chat/:name', async function(req, res){
 
-		let message = new Message(req.body.sender[i], req.body.message[i], req.body.embed[i]);
-		// Null the embed if it's nonexistent
-		if (message.embedPath == ''){
-			message.embedPath = null;
-		}
-		messages.push(message)
-	}
-	let quote = new Quote(messages);
-	let id = await QuoteDB.addQuote(quote);
-	res.redirect('/quote/' + id);
-});
+	const { Configuration, OpenAIApi } = require("openai");
 
-router.post('/removeQuote', urlencodedParser, async function(req, res){
-	if (req.body.password != config.adminPass) return res.send('Error: Invalid');
-	let result = await QuoteDB.removeQuote(req.body.id);
-	res.send(result);
-});
-
-router.get('/addQuote', function(req, res){
-	res.render('submit');
-});
-
-router.get('/removeQuote', function(req, res){
-	res.render('removeQuote');
-});
-
-router.get('/search', async function(req, res){
-	if (typeof req.query.query === 'undefined') return res.render('search');
-	await QuoteDB.searchQuotes(req.query.query).then((quotes) => {
-		return res.render('search', {quotes: quotes, query:req.query.query, avatars: config.avatars});
-	}).catch((err) => {
-		return res.send("Sorry, something went wrong. Try again later.");
+	const configuration = new Configuration({
+		apiKey: config.openai_key,
 	});
-});
+	const openai = new OpenAIApi(configuration);
 
-router.get('/profiles/:name', function(req, res){
-	res.redirect('/search?query=' + req.params.name);
-});
-
-router.get('/quote', function(req, res){
-	res.redirect('/quote/' + QuoteDB.getRandomID().toString());
-});
-
-router.get('/quote/:id', async function(req, res){
-	QuoteDB.getQuote(req.params.id).then((quote) => {
-		return res.render('quote', {quote: quote, id: req.params.id, avatars: config.avatars});
-	}).catch((err) => {
-		console.log(err);
-		return res.send('Sorry, that quote doesn\'t exist');
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [
+			{role: "system", content: config.prompt},
+			{role: "user", content: req.params.name}
+		],
 	});
-});
-
-router.get('/populateProfiles', async function(req, res){
-	res.send(ProfileDB.setJSONFile());
-});
-
-router.get('/persosona', function(req, res){
-	res.render('persosona');
+	res.send(completion.data.choices[0].message.content);
 });
 
 router.get('/*', async function(req, res){
